ae5acd978edbcfaff29f9f41f7bb5d70a7280b12
SwatiPrasad123 <Swatiprasad385@gmail.com>
2023-06-21 12:41:24 +0530

    first commit

---
+++ Controllers/WebController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChotanagpurWelfareFoundation.Controllers
+{
+    public class WebController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        public ActionResult Volunteer()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
